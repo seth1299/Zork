7e55a58ecfa5652bbbb7411160ad86565a1e18e6
seth1299 <59626391+seth1299@users.noreply.github.com>
2021-08-26 12:24:18 -0400

    Finished Module 1.2
    Finished step 2 of Module 1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,28 +2,42 @@
 
 namespace Zork
 {
+
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            string inputString = "";
+            Commands command = UNKNOWN;
+
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK")
+            inputString = Console.ReadLine();
+            command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+
+            if (inputString == "LOOK" || inputString == "L")
             {
                 Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying \"Welcome to Zork!\" lies by the door.");
             }
             else
             {
-                Console.WriteLine("Unrecognized command.");
+                Console.WriteLine("Unexpected command.");
             }
         }
-        
+
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
     }
 }
