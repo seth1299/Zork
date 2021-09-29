c48a414fc87812a3bdeb7c96c1ee3ed439aa7747
seth1299 <59626391+seth1299@users.noreply.github.com>
2021-09-29 15:16:25 -0400

    Made Rooms[,] no longer hard-coded in 4.2
    Rooms[,] is no longer hard-coded

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -117,12 +117,7 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static Room[,] Rooms =
-         {
-            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-            {new Room("Forest"), new Room("West of House"), new Room("Behind House")},
-            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing")}
-         };
+        private static Room[,] Rooms;
 
         private static void InitializeRooms(string roomsFilename) =>
             Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
