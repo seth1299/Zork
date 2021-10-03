8a0ec29034e1c9e757bd1a4d8e2acf22ea4ad18e
seth1299 <59626391+seth1299@users.noreply.github.com>
2021-10-03 17:04:51 -0400

    This will throw a NullReferenceException error
    NullReferenceExceptionErrors be like

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -3,135 +3,22 @@ using System.Collections.Generic;
 
 namespace Zork
 {
-
-    internal class Program
+    class Program
     {
-        private static Room CurrentRoom
-        {
-            get
-            {
-                return Rooms[Location.Row, Location.Column];
-            }
-        }
-
         static void Main(string[] args)
         {
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+            Game game = Game.Load(gameFilename);
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescriptions();
-
-            while (true)
-            {
-                Console.Write($"\n{CurrentRoom}\n>");
-
-                Commands command = ToCommand(Console.ReadLine().Trim());
-
-                if (command == Commands.QUIT)
-                {
-                    Console.Write("Thanks for playing!\n");
-                    break;
-                }
-
-
-                switch (command)
-                {
-
-                    case Commands.LOOK:
-                        Console.WriteLine(CurrentRoom.Description);
-                        break;
-
-                    case Commands.INVENTORY:
-                        Console.WriteLine("You are carrying a piece of paper with the words \"Up, up, down, down, left, right, left, right, B, A\" written on it. \nYou aren't sure what they mean.");
-                        break;
-
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                    
-                        if (Move(command) == false)
-                        {
-                            Console.WriteLine("The way is shut!");
-                        }
-                        break;
-
-                    case Commands.HELP:
-                        Console.WriteLine("Type \"NORTH\" or \"N\" to go NORTH, \"SOUTH\" or \"S\" to go SOUTH, \"WEST\" or \"W\" to go WEST, \"EAST\" or \"E\" to go EAST, \"QUIT\" or \"Q\" to " +
-                            "quit the game, \"I\" or \"INVENTORY\" to look in your inventory, \"C\", \"CLS\", or \"CLEAR\" to clear all of the messages on the Console, or \"L\" or \"LOOK\" to look around your current location.");
-                        break;
-
-                    default:
-                        Console.WriteLine("Unknown command.");
-                        break;
-                }
-            }
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
 
-        private static bool Move(Commands command)
+        private enum CommandLineArguments
         {
-            Assert.IsTrue(IsDirection(command), "Invalid direction.");
-
-            bool didMove = true;
-
-            switch (command)
-            {
-                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
-                    Location.Row++;
-                    break;
-
-                case Commands.SOUTH when Location.Row > 0:
-                    Location.Row--;
-                    break;
-
-                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-                    Location.Column++;
-                    break;
-
-                case Commands.WEST when Location.Column > 0:
-                    Location.Column--;
-                    break;
-
-                default:
-                    didMove = false;
-                    break;
-            }
-
-            return didMove;
+            GameFilename = 0
         }
-
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-        private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-        private static readonly Room[,] Rooms =
-         {
-            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-            {new Room("Forest"), new Room("West of House"), new Room("Behind House")},
-            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing")}
-         };
-
-        private static void InitializeRoomDescriptions()
-        {
-            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
-            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
-
-            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
-            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
-            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-
-            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
-        }
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST
-        };
-
-        private static (int Row, int Column) Location = (1, 1);
     }
 }
